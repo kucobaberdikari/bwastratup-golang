0cfad384643123cbfa92ee6084d968a64503039e
kucobaberdikari <kucobaberdikari@gmail.com>
2022-12-18 16:30:14 +0700

    upload image endpoint

--- campaign/repository.go
+++ campaign/repository.go
@@ -7,9 +7,11 @@ import (
 type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(userID int) ([]Campaign, error)
-	FindByID(ID int)(Campaign, error)
+	FindByID(ID int) (Campaign, error)
 	Save(campaign Campaign) (Campaign, error)
 	Update(campaign Campaign) (Campaign, error)
+	CreateImage(campaignImage CampaignImage) (CampaignImage, error)
+	MarkAllImagesAsNonPrimary(campaignID int) (bool, error)
 }
 
 type repository struct {
@@ -42,17 +44,17 @@ func (r *repository) FindByUserID(userID int) ([]Campaign, error) {
 	return campaigns, nil
 }
 
-func (r *repository) FindByID(ID int)(Campaign, error){
+func (r *repository) FindByID(ID int) (Campaign, error) {
 	var campaign Campaign
 
 	err := r.db.Preload("User").Preload("CampaignImages").Where("id = ?", ID).Find(&campaign).Error
-	if err != nil{
+	if err != nil {
 		return campaign, err
 	}
 	return campaign, nil
 }
 
-func(r *repository) Save(campaign Campaign) (Campaign, error){
+func (r *repository) Save(campaign Campaign) (Campaign, error) {
 	err := r.db.Create(&campaign).Error
 	if err != nil {
 		return campaign, err
@@ -67,6 +69,24 @@ func (r *repository) Update(campaign Campaign) (Campaign, error) {
 	if err != nil {
 		return campaign, err
 	}
- 
+
 	return campaign, nil
-} 
+}
+
+func (r *repository) CreateImage(campaignImage CampaignImage) (CampaignImage, error) {
+	err := r.db.Create(&campaignImage).Error
+	if err != nil {
+		return campaignImage, err
+	}
+
+	return campaignImage, nil
+}
+
+func (r *repository) MarkAllImagesAsNonPrimary(campaignID int) (bool, error){
+	err := r.db.Model(&CampaignImage{}).Where("campaign_id = ?", campaignID).Update("is_primary", false).Error
+
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
